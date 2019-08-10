a2ff4edb4e8552e4eae7ba5419fa2f77a1002754
suljaxm <ujxhz@outlook.com>
2019-08-10 23:31:52 +0800

    extending the OpenStreetMap data model

--- src/route_model.h
+++ src/route_model.h
@@ -12,6 +12,16 @@ class RouteModel : public Model {
     class Node : public Model::Node {
       public:
         // Add public Node variables and methods here.
+        Node * parent = nullptr;
+        float h_value = std::numeric_limits<float>::max();
+        float g_value = 0.0;
+        bool visited = false;
+        std::vector<Node *> neighbors;
+
+        float distance(Node other) const {
+          return std::sqrt(std::pow((x - other.x), 2) + std::pow((y - other.y), 2));
+        }
+        void FindNeighbors();
         
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
@@ -20,13 +30,20 @@ class RouteModel : public Model {
         // Add private Node variables and methods here.
         int index;
         RouteModel * parent_model = nullptr;
+        Node * FindNeighbor(std::vector<int> node_indices);
     };
     
     // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+    auto &SNodes(){ return m_Nodes; }
+    auto &GetNodeToRoadMap() { return node_to_road; }
+    Node &FindClosetNode(float x, float y);
 
   private:
     // Add private RouteModel variables and methods here.
+    std::vector<Node> m_Nodes;
+    void CreateNodeToRoadHashmap();
+    std::unordered_map<int, std::vector<const Model::Road *>> node_to_road;
 
 };
